6e9bb564063962193b3fc8637d6a2b98f0258804
pritampani <pritampani445@gmail.com>
2025-06-19 19:28:27 +0530

    chapter1 python primer

--- python/DSA_BOOK_MICHAEL_T_GOODRICH/Python_Primer/day1.py
+++ python/DSA_BOOK_MICHAEL_T_GOODRICH/Python_Primer/day1.py
@@ -8,13 +8,30 @@
 
 
 #1.2
-class Solution:
-    def is_even(self,n):
-        return n%2==0
+# class Solution:
+#     def is_even(self,n):
+#         return n%2==0
+
+# a=Solution()
+# print(a.is_even(10,2))
+
+
+#1.3
+
+class Solution():
+    def minmax(self,arr):
+        if len(arr)==0:
+            return (0,0)
+        mini,maxi=arr[0],arr[0]
+        for i in range(len(arr)):
+            if arr[i]>maxi:
+                maxi=arr[i]
+            if arr[i]<mini:
+                mini=arr[i]
+        return (mini,maxi)
 
 a=Solution()
-print(a.is_even(10,2))
-
+print(a.minmax([4,23,4,3,5,3,4,54,6,67,2,3,3,9]))
 
 
 
