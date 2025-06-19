53110358588f57540825b5dd787eca1343c36520
pritampani <pritampani445@gmail.com>
2025-06-19 17:53:42 +0530

    day 8

--- python/recursion/day8.py
+++ python/recursion/day8.py
@@ -62,27 +62,50 @@
 
 #write a python function which can show how many vowels and consonant are there in a string
 
-class Solution:
-    def countvowleandconsonant(self,s):
-        if s=='':
-            return (0,0)
-        i=len(s)-1
-        v=0
-        c=0
-        return self.helper(s,i,v,c)
-    def helper(self,s,i,v,c):
-        if i==-1:
-            return v,c
-        if s[i] in ('a','e','i','o','u','A','E','I','O','U'):
-            return self.helper(s,i-1,v+1,c)
-        elif s[i].isalpha():
-            return self.helper(s,i-1,v,c+1)
-        else:
-            return self.helper(s,i-1,v,c)
+# class Solution:
+#     def countvowleandconsonant(self,s):
+#         if s=='':
+#             return (0,0)
+#         i=len(s)-1
+#         v=0
+#         c=0
+#         return self.helper(s,i,v,c)
+#     def helper(self,s,i,v,c):
+#         if i==-1:
+#             return v,c
+#         if s[i] in ('a','e','i','o','u','A','E','I','O','U'):
+#             return self.helper(s,i-1,v+1,c)
+#         elif s[i].isalpha():
+#             return self.helper(s,i-1,v,c+1)
+#         else:
+#             return self.helper(s,i-1,v,c)
 
-a = Solution()
-print(a.countvowleandconsonant('aeiouqwrt'))  # (5,4)
-print(a.countvowleandconsonant('123'))        # (0,0)
-print(a.countvowleandconsonant('hello!'))     # (2,3)
-print(a.countvowleandconsonant(' hi '))       # (1,1)
-print(a.countvowleandconsonant('')) 
+# a = Solution()
+# print(a.countvowleandconsonant('aeiouqwrt'))  # (5,4)
+# print(a.countvowleandconsonant('123'))        # (0,0)
+# print(a.countvowleandconsonant('hello!'))     # (2,3)
+# print(a.countvowleandconsonant(' hi '))       # (1,1)
+# print(a.countvowleandconsonant('')) 
+
+
+
+
+# write a python function so that it will rearrange all the even value 
+
+class Solution:
+    def rearrange(self,arr):
+        if len(arr)<=1:
+            return []
+        i=0
+        j=len(arr)-1
+        return self.helper(arr,i,j)
+    def helper(self,arr,i,j):
+        if i>=j:
+            return arr
+        if arr[j]%2==0 and arr[i]%2!=0:
+            arr[j],arr[i]=arr[i],arr[j]
+            return self.helper(arr,i+1,j-1)
+        else:
+            return self.helper(arr,i+1,j-1)
+a=Solution()
+print(a.rearrange([1,9,2,8,3,7,4,6,5]))
