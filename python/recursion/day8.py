c2fb1dcf6fb5874c724c9bfc27f706bbd5c38cf6
pritampani <pritampani445@gmail.com>
2025-06-19 12:06:12 +0530

    day 8

--- python/recursion/day8.py
+++ python/recursion/day8.py
@@ -1,18 +1,34 @@
-# wite a recursive function that reverse the string
+# # wite a recursive function that reverse the string
 
-class Solution:
-    def reverse(self,s):
-        if s=='':
-            return s
-        i=len(s)-1
-        res=''
-        return self.helper(s,i,res)
+# class Solution:
+#     def reverse(self,s):
+#         if s=='':
+#             return s
+#         i=len(s)-1
+#         res=''
+#         return self.helper(s,i,res)
 
-    def helper(self,s,i,res):
-        if i==-1:
-            return res
-        return self.helper(s,i-1,res+s[i])
-a=Solution()
-print(a.reverse('chiku'))
+#     def helper(self,s,i,res):
+#         if i==-1:
+#             return res
+#         return self.helper(s,i-1,res+s[i])
+# a=Solution()
+# print(a.reverse('chiku'))
 
+#O(n) time and space efficient.
+# class Solution:
+#     def reverse(self, s):
+#         if s == '':
+#             return s
+#         res = []
+#         self.helper(s, len(s) - 1, res)
+#         return ''.join(res)
 
+#     def helper(self, s, i, res):
+#         if i == -1:
+#             return
+#         res.append(s[i])
+#         self.helper(s, i - 1, res)
+
+# a = Solution()
+# print(a.reverse('chiku'))  # Output: 'ukihc'
