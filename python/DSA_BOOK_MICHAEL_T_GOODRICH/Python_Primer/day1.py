b436bb4e8698a9089041d8951505082de26df949
pritampani <pritampani445@gmail.com>
2025-06-19 19:37:13 +0530

    chapter1 python primer

--- python/DSA_BOOK_MICHAEL_T_GOODRICH/Python_Primer/day1.py
+++ python/DSA_BOOK_MICHAEL_T_GOODRICH/Python_Primer/day1.py
@@ -18,20 +18,34 @@
 
 #1.3
 
-class Solution():
-    def minmax(self,arr):
-        if len(arr)==0:
-            return (0,0)
-        mini,maxi=arr[0],arr[0]
-        for i in range(len(arr)):
-            if arr[i]>maxi:
-                maxi=arr[i]
-            if arr[i]<mini:
-                mini=arr[i]
-        return (mini,maxi)
+# class Solution():
+#     def minmax(self,arr):
+#         if len(arr)==0:
+#             return (0,0)
+#         mini,maxi=arr[0],arr[0]
+#         for i in range(len(arr)):
+#             if arr[i]>maxi:
+#                 maxi=arr[i]
+#             if arr[i]<mini:
+#                 mini=arr[i]
+#         return (mini,maxi)
 
-a=Solution()
-print(a.minmax([4,23,4,3,5,3,4,54,6,67,2,3,3,9]))
+# a=Solution()
+# print(a.minmax([4,23,4,3,5,3,4,54,6,67,2,3,3,9]))
+
+
+#1.4
+
+# class Solution:
+#     def sumofallthesqure(self,n):
+#         sumo=0
+#         for i in range(1,n):
+#             sumo+=i**2
+#         return sumo
+    
+# a=Solution()
+# print(a.sumofallthesqure(3))
+
 
 
 
