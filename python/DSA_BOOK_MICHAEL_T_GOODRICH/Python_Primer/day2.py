0fc4d9ba84cbf62405248e5e3c4a96680dd1b92e
pritampani <pritampani445@gmail.com>
2025-06-20 12:52:12 +0530

    chapter1 python primer day2

--- python/DSA_BOOK_MICHAEL_T_GOODRICH/Python_Primer/day2.py
+++ python/DSA_BOOK_MICHAEL_T_GOODRICH/Python_Primer/day2.py
@@ -10,21 +10,38 @@
 
 #1.14]
 
+# class Solution:
+#     def checkdistinct(self,arr):
+#         if len(arr)==0:
+#             return set()
+#         seen=set()
+#         for i in range(len(arr)):
+#             for j in range(len(arr)):
+#                 if (arr[i]*arr[j])%2!=0 and (arr[i],arr[j]) not in seen:
+#                     seen.add((arr[i],arr[j]))
+#         return seen
+# a=Solution()
+# print(a.checkdistinct([1,2,3,4,5,6,7,7,8,9]))
+
+
+
+#1.15
 class Solution:
-    def checkdistinct(self,arr):
+    def checkforelemntrepetations(self,arr):
         if len(arr)==0:
-            return set()
+            return 'no elemnet is in the array'
         seen=set()
         for i in range(len(arr)):
-            for j in range(len(arr)):
-                if (arr[i]*arr[j])%2!=0 and (arr[i],arr[j]) not in seen:
-                    seen.add((arr[i],arr[j]))
-        return seen
+            if arr[i] not in seen:
+                seen.add(arr[i])
+            else:
+                return 'element is repeted'
+        return 'no element is repeting'
 a=Solution()
-print(a.checkdistinct([1,2,3,4,5,6,7,7,8,9]))
 
-
-
+print(a.checkforelemntrepetations([1,2,3,4,5,6,7]))
+print(a.checkforelemntrepetations([1,2,3,4,5,6,6,7]))
+print(a.checkforelemntrepetations([]))
 
 
 
