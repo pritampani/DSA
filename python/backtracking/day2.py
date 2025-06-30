8495f90f845e5cd577bbc48b2287e0baddbe3195
pritampani <pritampani445@gmail.com>
2025-06-30 18:37:10 +0530

    backtracking day2

--- python/backtracking/day2.py
+++ python/backtracking/day2.py
@@ -21,3 +21,36 @@
 # a=Solution()
 # print(a.permute([1,2,3]))
 # print(a.permute([1,2]))
+
+
+
+
+#47. Permutations II
+from typing import List
+class Solution:
+    def permuteUnique(self, arr: List[int]) -> List[List[int]]:
+        i=0
+        res=[]
+        self.helper(arr,i,res)
+        return res
+    def helper(self,arr,i,res):
+        if i==len(arr):
+            res.append(arr[:])
+            return
+        seen=set()
+        for idx in range(i,len(arr)):
+            if arr[idx] in seen:
+                continue
+            seen.add(arr[idx])
+            arr[i],arr[idx]=arr[idx],arr[i]
+            self.helper(arr,i+1,res)
+            arr[i],arr[idx]=arr[idx],arr[i]
+
+a=Solution()
+print(a.permuteUnique([1,2,3]))
+print(a.permuteUnique([1,2]))
+
+
+
+
+
