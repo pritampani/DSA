e6ba7a63f27924808327b5fac9c22d0a1a83576d
pritampani <pritampani445@gmail.com>
2025-07-05 12:34:44 +0530

    stack day1

--- python/stack/day1.py
+++ python/stack/day1.py
@@ -42,3 +42,27 @@
 # print(a.nextsmallerelement([1,2,3,4,5,6,7,8,9]))
 # print(a.nextsmallerelement([10,9,8,7,6,5,4,3,2,1]))
 
+
+#previous greter element
+
+# class Solution:
+#     def previoussmallerelement(self,arr):
+#         stk=[]
+#         ans=[]
+#         for i in range(len(arr)):
+#             while stk and stk[-1]>=arr[i]:
+#                 stk.pop()
+#             if not stk:
+#                 ans.append(-1)
+#             else:
+#                 ans.append(stk[-1])
+#             stk.append(arr[i])
+
+#         return ans
+# a=Solution()
+# print(a.previoussmallerelement([5,3,6,4,2,3,9,10]))
+# print(a.previoussmallerelement([1,2,3,4,5,6,7,8,9]))
+# print(a.previoussmallerelement([10,9,8,7,6,5,4,3,2,1]))
+
+
+
