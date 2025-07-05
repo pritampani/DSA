6698b68da557ff56cdf2cb2ae658abdf3fe02625
pritampani <pritampani445@gmail.com>
2025-07-05 12:34:58 +0530

    stack day1

--- python/stack/day1.py
+++ python/stack/day1.py
@@ -66,3 +66,22 @@
 
 
 
+#previougreaterelement
+class Solution:
+    def previougreaterelement(self,arr):
+        stk=[]
+        ans=[]
+        for i in range(len(arr)):
+            while stk and stk[-1]<=arr[i]:
+                stk.pop()
+            if not stk:
+                ans.append(-1)
+            else:
+                ans.append(stk[-1])
+            stk.append(arr[i])
+
+        return ans
+a=Solution()
+print(a.previougreaterelement([5,3,6,4,2,3,9,10]))
+print(a.previougreaterelement([1,2,3,4,5,6,7,8,9]))
+print(a.previougreaterelement([10,9,8,7,6,5,4,3,2,1]))
