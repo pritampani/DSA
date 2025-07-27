10f0416b90e94d447257bd2543f37b6da096fbb3
pritampani <pritampani445@gmail.com>
2025-07-27 15:55:08 +0530

    hashing day1

--- python/hashing/day1.py
+++ python/hashing/day1.py
@@ -18,7 +18,27 @@ class Solution:
                 c+=1
                 seen.add(s[i])
         return c
-        
+
+       
+# TC=O(N)
+# AS=O(N)     
+class Solution:
+    def getCount (self,s, n):
+        freq={}
+        prev=''
+        for i in range(len(s)):
+            if s[i]!=prev:
+                freq[s[i]] = freq.get(s[i], 0) + 1
+            prev=s[i]
+        c=0
+        seen=set()
+        for i in range(len(s)):
+            if freq[s[i]]==n and s[i] not in seen:
+                c+=1
+                seen.add(s[i])
+                
+                
+        return c
 
 a=Solution()
 print(a.getCount('geeksforgeeks'))
