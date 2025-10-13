c5d28fa29e10d5d299159dab150e1c20f328d181
pritampani <pritampani445@gmail.com>
2025-10-13 17:16:37 +0530

    problem 9

--- python/SELF_challanges/OCT_13_2025/p9.py
+++ python/SELF_challanges/OCT_13_2025/p9.py
@@ -1,3 +1,20 @@
 # Given a string with “ranges” like "1-5,10-12,20-20", expand them to list of numbers: [1,2,3,4,5,10,11,12,20].
 
 
+
+class Solution:
+    def p9(self, s):
+        res = []
+        parts = s.split(',')           
+        for part in parts:
+            a, b = part.split('-')    
+            a, b = int(a), int(b)
+            res += self.rangearray(a, b)
+        return res
+
+    def rangearray(self, a, b):
+        return [i for i in range(a, b + 1)] 
+
+a = Solution()
+s = "1-5,10-12,20-20"
+print(a.p9(s))
