794195bc10e62ec62431de5204fea55b3d03e0e8
pritampani <pritampani445@gmail.com>
2025-10-13 14:57:18 +0530

    problem 5

---
+++ python/SELF_challanges/OCT_13_2025/p5.py
@@ -0,0 +1,17 @@
+
+# Given a string with words and numbers mixed, find the sum of all numbers in the string.
+
+class Solution():
+    def p4(self,s):
+        
+        c=0
+        for i in range(len(s)):
+            if s[i].isdigit():
+                c+=int(s[i])
+
+        return c
+
+a=Solution()
+
+s='jj222j4'
+print(a.p4(s))
