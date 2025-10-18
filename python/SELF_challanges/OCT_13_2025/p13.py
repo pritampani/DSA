3e54ddc74478fdf4164df33f2bb217deb3037fa0
pritampani <pritampani445@gmail.com>
2025-10-18 16:23:45 +0530

    problem 13 optimised

--- python/SELF_challanges/OCT_13_2025/p13.py
+++ python/SELF_challanges/OCT_13_2025/p13.py
@@ -6,16 +6,46 @@ Input
 Expected Output
 "revolutionizing"
 """
+#tc=O(n)
+#AS=O(n)
+# class Solution:
+#     def p13(self,s):
+#         k=s.split(' ')
+#         mxstr=''
+#         for i in range(len(k)):
+#             if len(k[i])>len(mxstr):
+#                 mxstr=k[i]
+#         return mxstr
+# a=Solution()
+# s="Artificial intelligence is revolutionizing everything!"
+# print(a.p13(s))
+
+
+# tc=O(n)
+# AS=O(1)
 
 class Solution:
     def p13(self,s):
-        k=s.split(' ')
+        
         mxstr=''
-        for i in range(len(k)):
-            if len(k[i])>len(mxstr):
-                mxstr=k[i]
+        i=0
+        curr=''
+        while i<len(s):
+            if s[i].isalpha():
+                curr+=s[i]
+                i+=1
+            else:
+                if curr!='' and len(curr)>len(mxstr):
+                    mxstr=curr
+                    curr=''
+                    
+                curr=''
+                i+=1
+
+
+
         return mxstr
 a=Solution()
 
-s="Artificial intelligence is revolutionizing everything!"
+s="Artificial intelligence is revolutionizing everythingdfgssefgfg!"
 print(a.p13(s))
