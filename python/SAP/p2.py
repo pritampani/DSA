1a0f5500a0333a2f78d283c521febad0ae851df8
pritampani <pritampani445@gmail.com>
2025-10-28 14:14:30 +0530

    SAP preprations

--- python/SAP/p2.py
+++ python/SAP/p2.py
@@ -4,4 +4,13 @@
 
 
 class Solution:
-    def check_palandrom
+    def check_palandrom(self,n):
+        a=n
+        res=0
+        while a>0:
+            d=a%10
+            res=res*10+d
+            a//=10
+        return n==res
+a=Solution()
+print(a.check_palandrom(1213))
