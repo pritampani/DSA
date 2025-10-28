35fb7e7289c1edff9b139c5040ebcc9b786f7b43
pritampani <pritampani445@gmail.com>
2025-10-28 15:50:33 +0530

    SAP preprations

--- python/SAP/p15.py
+++ python/SAP/p15.py
@@ -11,6 +11,26 @@ class Solution:
             for j in range(i-1,0,-1):
                 print(j,end="")
             print()
+    def num_diamond(self,n):
+        for i in range(1,n+1):
+            print(" "*(n-i),end=' ')
+
+            for j in range(1,i+1):
+                print(j,end='')
+            for j in range(i-1,0,-1):
+                print(j,end='')
+            print()
+        
+        for i in range(n-1,0,-1):
+            print(" "*(n-i),end=' ')    
+            for j in range(1,i+1):
+                print(j,end='')
+            for j in range(i-1,0,-1):
+                print(j,end='')
+            print()
+
 a=Solution()
-a.num_primyed(7)
+#a.num_primyed(7)
+a.num_diamond(7)
+
 
