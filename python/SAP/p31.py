20c8a4b383c05d9d207fd5ca61ec4a425dc56096
pritampani <pritampani445@gmail.com>
2025-10-28 17:49:21 +0530

    SAP preprations

--- python/SAP/p31.py
+++ python/SAP/p31.py
@@ -1,5 +1,19 @@
 #Binary Search (iterative)
 
 class Solution:
-    def binary_search(self,arr):
-        
+    def binary_search(self,arr,target):
+        l=0
+        r=len(arr)-1
+        while l<=r:
+            mid=(l+r)//2
+            if arr[mid]==target:
+                return mid
+            if arr[mid]>target:
+                r=mid-1
+            else:
+                l=mid+1
+        return -1
+
+a=Solution()
+print(a.binary_search([1,2,3,4,5,6,7,8,9],7))
+
