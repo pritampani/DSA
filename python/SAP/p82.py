818ea603ccfa329118b2f3467d3ac5a3a54f2aae
pritampani <pritampani445@gmail.com>
2025-10-29 17:26:35 +0530

    SAP preprations

---
+++ python/SAP/p82.py
@@ -0,0 +1,15 @@
+# previous greater element
+
+class Solution:
+    def PGE(self,arr):
+        res=[-1]*len(arr)
+        stk=[]
+        for i in range(len(arr)):
+            while stk and stk[-1]<=arr[i]:
+                stk.pop()
+            if stk:
+                res[i]=stk[-1]
+            stk.append(arr[i])
+        return res
+a = Solution()
+print(a.PGE([10, 4, 2, 20, 40, 12, 30]))
